596d8f65d2996e6c8ae448347e67a66689486504
jason king <pizza@netspace.net.au>
2019-01-31 11:44:16 +1100

    fix default flake8 compliance issues.  I'm not a fan of 80 char limits while using 1920x1080 screens.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import requests
 centimetre_to_metre_conversion = 0.01
 
 # So the data has a "weight" column, but the exercise specifies a value of 250
-# DHL specifies a conversion factor of 250 or 333 (as does Aust Post) , so conversion
-# factor seems to be for all packages, not just air conditioners.
+# DHL specifies a conversion factor of 250 or 333 (as does Aust Post) , so
+# conversion factor seems to be for all packages, not just air conditioners.
 # DHL does demand you use the maximum of w*l*h*conv , or specified weight
 cubic_weight_conversion_factor = 250
 
@@ -25,7 +25,8 @@ while not reached_end:
 
     # have to massage the json a bit before converting to dataframe
     # and flatten a few internal dicts
-    dataframe_page = pandas.io.json.json_normalize(json.loads(data.text)['objects'])
+    dataframe_page = \
+        pandas.io.json.json_normalize(json.loads(data.text)['objects'])
     pages.append(dataframe_page)
 
     if data_converted['next']:
@@ -34,18 +35,20 @@ while not reached_end:
         reached_end = True
 
 
-# convert to a single dataframe.  Appending rows to a dataframe creates a new dataframe every time,
-# and using the DataFrame.concat() function a lot is quite wasteful, so concat all at once
+# convert to a single dataframe.  Appending rows to a dataframe creates a new
+# dataframe every time, and using the DataFrame.concat() function a lot is
+# quite wasteful, so concat all at once
 
 all_data = pandas.concat(pages)
-all_data['cubic_weight'] = all_data['size.height'] * centimetre_to_metre_conversion *\
-                           all_data['size.length'] * centimetre_to_metre_conversion *\
-                           all_data['size.width'] * centimetre_to_metre_conversion * \
-                           cubic_weight_conversion_factor
+all_data['cubic_weight'] = \
+    all_data['size.height'] * centimetre_to_metre_conversion *\
+    all_data['size.length'] * centimetre_to_metre_conversion *\
+    all_data['size.width'] * centimetre_to_metre_conversion * \
+    cubic_weight_conversion_factor
 
 # now get the average of what we're interested in
 air_conditioners = all_data.loc[all_data['category'] == 'Air Conditioners']
 count = len(air_conditioners.index)
 total = air_conditioners['cubic_weight'].sum()
-print('average cubic weight of air conditioners is ',  '%.2f' % (total/count), ' kg')
-
+print('average cubic weight of air conditioners is ',
+      '%.2f' % (total/count), ' kg')
